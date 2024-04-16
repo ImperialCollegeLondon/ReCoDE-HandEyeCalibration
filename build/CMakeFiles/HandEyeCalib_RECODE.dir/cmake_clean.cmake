b266ccea3215b3856e571f2b5582ac80dc902e51
ERIC_Z_CUI <931734063@qq.com>
2024-04-16 18:21:48 +0100

    Improve Compilation for Windows

---
+++ build/CMakeFiles/HandEyeCalib_RECODE.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HandEyeCalib_RECODE.dir/FeatureDetection.cc.o"
+  "CMakeFiles/HandEyeCalib_RECODE.dir/FeatureDetection.cc.o.d"
+  "CMakeFiles/HandEyeCalib_RECODE.dir/main.cc.o"
+  "CMakeFiles/HandEyeCalib_RECODE.dir/main.cc.o.d"
+  "CMakeFiles/HandEyeCalib_RECODE.dir/utils.cc.o"
+  "CMakeFiles/HandEyeCalib_RECODE.dir/utils.cc.o.d"
+  "HandEyeCalib_RECODE"
+  "HandEyeCalib_RECODE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HandEyeCalib_RECODE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
